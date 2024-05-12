bfccab8ee99094a0b092a22a002450fb3465ca22
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 22:13:35 +0200

    Add project files.

---
+++ Ex_w_2_l_7_4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace W_2_l_7_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Year;
+            Console.WriteLine("Podaj rok");
+
+            Year = int.Parse(Console.ReadLine());
+
+            if (Year % 4 == 0)
+            {
+                Console.WriteLine(Year + " jest rokiem przestępnym");
+            }
+            else
+            {
+                Console.WriteLine(Year + " rok nie jest rokiem przestępnym");
+            }
+        }
+    }
+}
